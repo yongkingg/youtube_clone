3eb9a9c36a7fe8a485a0bae3d8aeddb967412264
yongkingg <dydwns8064@naver.com>
2024-09-07 21:16:39 +0900

    First commit

---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import Aside from "../Widget/Aside";
+
+const App = () => {
+  return (
+    <>
+      <Aside />
+      <h1>하이요</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Widget/Aside/index.js
@@ -0,0 +1,7 @@
+import styles from "./style/aside.module.css";
+
+const Aside = () => {
+  return <h1 className={styles.title}>반갑습니다</h1>;
+};
+
+export default Aside;
